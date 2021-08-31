b0f4fdad0486f01261c46ab8b3dc400973f80ca4
Daiki-Muroga <daiki.muroga@terra-drone.co.jp>
2021-08-31 19:10:16 +0900

    display events

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import reducer from '../reducers'
+import Event from './Event'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
@@ -15,6 +16,9 @@ const App = () => {
     })
     setTitle('')
     setBody('')
+  }
+  const handleClickDeleteButton = e => {
+
   }
   return (
     <div className="container-fluid">
@@ -31,17 +35,23 @@ const App = () => {
 
         <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
-
-        <h4>イベント一覧</h4>
-        <table className="table table-hover">
-          <thead>
+      </form>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
             <th>ID</th>
             <th>タイトル</th>
             <th>ボディー</th>
             <th></th>
-          </thead>
-        </table>
-      </form>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
